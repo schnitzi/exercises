39d3630065ffb388e0c1b0ef5fb463fe88c4314f
Mark Schnitzius <mark.schnitzius@cxense.com>
2014-08-21 16:06:56 +1000

    more test cases

--- src/test/java/com/cxense/PreferencesUnitTest.java
+++ src/test/java/com/cxense/PreferencesUnitTest.java
@@ -12,13 +12,26 @@ public class PreferencesUnitTest {
 
 
     @Test
-    public void testExampleData() throws Exception {
+    public void testExamples() throws Exception {
 
-        final Preferences preferences = new Preferences(new File("src/test/resources/rostering.sample.in"));
+//        doTest("src/test/resources/rostering.easy.in", 0);
+        doTest("src/test/resources/rostering.impossible1.in", -1);
+        doTest("src/test/resources/rostering.sample.in", 3);
+    }
+
+    private void doTest(final String filename, final int expectedPain) throws Exception {
+
+        System.out.println("Testing " + filename);
+        final Preferences preferences = new Preferences(new File(filename));
         final Rostering rostering = new Rostering(preferences);
         Schedule schedule = rostering.findBestSchedule();
-        Assert.assertNotNull(schedule);
-        Assert.assertEquals(schedule.getPain(), 0);
-        System.out.println(schedule);
+        if (expectedPain == -1) {
+            // Not expected to be solvable.
+            Assert.assertNull(schedule, "Expected no solution for " + filename);
+        } else {
+            Assert.assertNotNull(schedule, "Expected a solution for " + filename);
+            Assert.assertEquals(schedule.getPain(), expectedPain, "Unexpected pain value");
+            System.out.println(schedule);
+        }
     }
 }
